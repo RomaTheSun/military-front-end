b537c8703d1eb185bf252c7552085d45954cfd9f
RomaTheSun <pryimak.roma@gmail.com>
2024-12-17 14:14:41 +0200

    Update register.tsx

--- app/register.tsx
+++ app/register.tsx
@@ -86,6 +86,7 @@ export default function RegisterScreen() {
         setDate(currentDate);
     };
 
+
     const showDatepicker = () => {
         setShowDatePicker(true);
     };
@@ -219,6 +220,7 @@ const styles = StyleSheet.create({
     },
     registerContainer: {
         margin: 20,
+        paddingTop: 40
     },
     maincolor: {
         color: "#344939",
